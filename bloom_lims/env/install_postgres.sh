0600384232d3265082538d4d9d638cdb63386b18
John Major <daylily@daylilyinformatics.com>
2025-07-02 18:04:11 -0700

    X

--- bloom_lims/env/install_postgres.sh
+++ bloom_lims/env/install_postgres.sh
@@ -8,7 +8,7 @@ PGPASSWORD=${PGPASSWORD:-passw0rd}
 export PGDBNAME=${PGDBNAME:-bloom}
 
 
-bash bloom_lims/bin/install_miniconda
+source bloom_lims/bin/install_miniconda
 
 sleep 5
 # Create a Conda environment named BLOOM if $1 is not set
